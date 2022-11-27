10f9230bb2c01b13a0ced099a99cbba3e088be77
Mohammad Arif <arif_iq@yahoo.co.in>
2022-11-27 15:02:03 +0530

    Add login and create account pages with Firebase Auth

--- blog/src/index.js
+++ blog/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAeCVMwAwzdD7XSjPLJQxmrGtC7qtzqWE8',
+  authDomain: 'my-react-blog-4f0e0.firebaseapp.com',
+  projectId: 'my-react-blog-4f0e0',
+  storageBucket: 'my-react-blog-4f0e0.appspot.com',
+  messagingSenderId: '158751897437',
+  appId: '1:158751897437:web:b67da75de506b2f0235ab4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
---
+++ blog/src/pages/LoginPage.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate('/articles');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Log In</h1>
+      {error && <p className='error'>{error}</p>}
+      <input
+        placeholder='Your email address'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder='Your password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type='password'
+      />
+      <button onClick={login}>Log In</button>
+      <Link to='/create-account'>Sign Up</Link>
+    </>
+  );
+};
+
+export default LoginPage;
